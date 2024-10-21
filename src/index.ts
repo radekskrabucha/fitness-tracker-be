44a52bd13b474228d9896a4382305f7424fdb42b
radekskrabucha <rskrabucha13@gmail.com>
2024-10-21 11:29:13 +0200

    removed tasks router

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,11 @@
 import { serve } from '@hono/node-server'
 import { createApp } from '~/lib/createApp'
 import { configureOpenApi } from '~/lib/openApi'
-import { tasksRouter } from '~/routes/tasks'
 import { env } from '~/utils/env'
 
 export const app = createApp()
 
 configureOpenApi(app)
-
-app.route('/tasks', tasksRouter)
 
 serve({
   fetch: app.fetch,
