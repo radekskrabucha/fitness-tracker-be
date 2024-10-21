7a40561575455117c74d122ce143fdf953e68c85
radekskrabucha <rskrabucha13@gmail.com>
2024-10-21 11:23:08 +0200

    added auth db schemas, adjusted drizzle config for multi files schema

--- src/types/app.ts
+++ src/types/app.ts
@@ -1,9 +1,12 @@
 import type { OpenAPIHono, RouteConfig, RouteHandler } from '@hono/zod-openapi'
 import type { PinoLogger } from 'hono-pino'
+import type { SelectSession, SelectUser } from '~/db/schema/auth.schema'
 
 export type AppBindings = {
   Variables: {
     logger: PinoLogger
+    user: SelectUser
+    session: SelectSession
   }
 }
 
