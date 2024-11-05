9d0c20c3e79afe4c64bdd200d10e75c161d4d172
radekskrabucha <rskrabucha13@gmail.com>
2024-11-05 11:19:15 +0100

    feat(auth): added trusted origins

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,7 @@ import { betterAuth } from 'better-auth'
 import { drizzleAdapter } from 'better-auth/adapters/drizzle'
 import { admin } from 'better-auth/plugins'
 import { db } from '~/db'
+import { env } from '~/utils/env'
 
 export const auth = betterAuth({
   database: drizzleAdapter(db, {
@@ -9,6 +10,7 @@ export const auth = betterAuth({
     generateId: false
   }),
   plugins: [admin()],
+  trustedOrigins: [env.BETTER_AUTH_TRUSTED_ORIGINS],
   emailAndPassword: {
     enabled: true
   },
