452b7b54a4749617c62de3249b9450e10b4f6b45
radekskrabucha <rskrabucha13@gmail.com>
2024-11-12 11:52:18 +0100

    refactor(zod-schemas): moved muscle groups category to new db schemas inferring

---
+++ src/lib/dbSchemaNew/muscleGroups.ts
@@ -0,0 +1,19 @@
+import { createInsertSchema, createSelectSchema } from 'drizzle-zod'
+import { z } from 'zod'
+import { muscleGroups } from '~/db/schema/exercise.schema'
+
+export const insertMuscleGroupSchema = createInsertSchema(muscleGroups, {
+  name: schema => schema.name.min(1).max(256)
+}).omit({
+  id: true,
+  createdAt: true,
+  updatedAt: true
+})
+export type InsertMuscleGroup = z.infer<typeof insertMuscleGroupSchema>
+
+export const patchMuscleGroupSchema = insertMuscleGroupSchema.partial()
+export type PatchMuscleGroup = z.infer<typeof patchMuscleGroupSchema>
+
+export const selectMuscleGroupSchema =
+  createSelectSchema(muscleGroups).openapi('MuscleGroup')
+export type SelectMuscleGroup = z.infer<typeof selectMuscleGroupSchema>
