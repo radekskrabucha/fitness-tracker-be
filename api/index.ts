6615e1529381a420523fa9ff69abafd28e522859
radekskrabucha <rskrabucha13@gmail.com>
2024-12-18 13:52:23 +0100

    feat(vercel): fixed import

--- api/index.ts
+++ api/index.ts
@@ -1,5 +1,5 @@
 import { handle } from "@hono/node-server/vercel"
 // @ts-expect-error - we need to import the app from the dist folder
-import app from "../dist/src/app.js"
+import app from "../dist/index.js"
 
 export default handle(app)
