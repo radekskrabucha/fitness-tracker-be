6bc05596d77c05b758f1eb21cf76323ac89e8936
radekskrabucha <rskrabucha13@gmail.com>
2024-12-30 11:05:05 +0100

    fix(OnErrorMiddleware): fixed status code type

--- src/middleware/onError.ts
+++ src/middleware/onError.ts
@@ -1,5 +1,5 @@
 import type { ErrorHandler } from 'hono'
-import type { StatusCode } from 'hono/utils/http-status'
+import type { ContentfulStatusCode } from 'hono/utils/http-status'
 import { env } from '~/utils/env'
 import { INTERNAL_SERVER_ERROR, OK } from '~/utils/httpCodes'
 
@@ -7,8 +7,11 @@ export const onError: ErrorHandler = (err, c) => {
   const currentStatus =
     'status' in err ? err.status : c.newResponse(null).status
   const statusCode =
-    currentStatus !== OK ? (currentStatus as StatusCode) : INTERNAL_SERVER_ERROR
+    currentStatus !== OK
+      ? (currentStatus as ContentfulStatusCode)
+      : INTERNAL_SERVER_ERROR
   const nodeEnv = c.env?.NODE_ENV || env.NODE_ENV
+
   return c.json(
     {
       message: err.message,
